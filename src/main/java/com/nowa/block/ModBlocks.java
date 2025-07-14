9d7fe912ab5dd119f98960327173fc6c787daa0f
Julien <103409256+Julien-Gelles@users.noreply.github.com>
2025-07-14 03:17:50 +0200

    [PR-5] Cleanup (#5)

--- src/main/java/com/nowa/block/ModBlocks.java
+++ src/main/java/com/nowa/block/ModBlocks.java
@@ -16,8 +16,8 @@ import static com.nowa.TemplateMod.MOD_ID;
 public class ModBlocks {
 
     public static final Block RUBY_BLOCK = registerBlock("ruby_block", new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
-    public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block", new Block(FabricBlockSettings.copyOf(Blocks.RAW_IRON_BLOCK)));
-    public static final Block DEEPSLATE_RAW_RUBY_BLOCK = registerBlock("deepslate_raw_ruby_block", new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_IRON_ORE)));
+    public static final Block RUBY_ORE = registerBlock("ruby_ore", new Block(FabricBlockSettings.copyOf(Blocks.RAW_IRON_BLOCK)));
+    public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore", new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_IRON_ORE)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
--- src/main/java/com/nowa/item/ModItemGroups.java
+++ src/main/java/com/nowa/item/ModItemGroups.java
@@ -4,7 +4,6 @@ import com.nowa.block.ModBlocks;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
@@ -21,8 +20,8 @@ public class ModItemGroups {
                         entries.add(ModItems.RUBY);
                         entries.add(ModItems.RAW_RUBY);
                         entries.add(ModBlocks.RUBY_BLOCK);
-                        entries.add(ModBlocks.RAW_RUBY_BLOCK);
-                        entries.add(ModBlocks.DEEPSLATE_RAW_RUBY_BLOCK);
+                        entries.add(ModBlocks.RUBY_ORE);
+                        entries.add(ModBlocks.DEEPSLATE_RUBY_ORE);
 
                     }).build());
 
