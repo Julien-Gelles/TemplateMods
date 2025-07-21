6272c7376e0e1b0c3751f2e972dba4f2bfcf6ae8
Julien <103409256+Julien-Gelles@users.noreply.github.com>
2025-07-21 10:54:03 +0200

    [PR-7] Shiny Depths

---
+++ src/main/java/com/nowa/ShinyDepths.java
@@ -0,0 +1,22 @@
+package com.nowa;
+
+import com.nowa.block.ModBlocks;
+import com.nowa.item.ModItemGroups;
+import com.nowa.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ShinyDepths implements ModInitializer {
+	public static final String MOD_ID = "shiny-depths";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+	}
+}
---
+++ src/main/java/com/nowa/datagen/ModModelProvider.java
@@ -0,0 +1,28 @@
+package com.nowa.datagen;
+
+import com.nowa.block.ModBlocks;
+import com.nowa.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RUBY_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RUBY_ORE);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.DEEPSLATE_RUBY_ORE);
+
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(ModItems.RUBY, Models.GENERATED);
+    }
+}
